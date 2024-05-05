5b1f5197abe9a501cf12fe00a0ae2f044e194ba2
aydinSerbest <aydinserbest34@gmail.com>
2024-05-05 12:27:48 +0200

    updated

---
+++ src/test/java/net/bddtrader/stepdefinitions/NewsStepDefinitions.java
@@ -0,0 +1,24 @@
+package net.bddtrader.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NewsStepDefinitions {
+    @Given("Tracy has a trading account")
+    public void tracy_has_a_trading_account() {
+
+    }
+    @Given("the following articles have been published:")
+    public void the_following_articles_have_been_published(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+    @When("she views he latest news rticles")
+    public void she_views_he_latest_news_rticles() {
+
+    }
+    @Then("the articles should be presented as follows:")
+    public void the_articles_should_be_presented_as_follows(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+}
